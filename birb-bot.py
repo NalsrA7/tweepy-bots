2a9b02edbe42078483439f7af7c4fe1ce845d0e7
Arslan Ahmad <arslanshero@gmail.com>
2021-10-12 20:42:47 +0700

    added saving tweets to json

--- birb-bot.py
+++ birb-bot.py
@@ -1,5 +1,7 @@
 import tweepy
 import json
+import time
+import json
 
 with open("tokens.json","r") as f:
     tokens = json.load(f)
@@ -21,23 +23,42 @@ except:
     print("Error during authentication")
     
 class MyStreamListener(tweepy.StreamListener):
-    def __init__(self, api):
+    def __init__(self, api, time_limit=60):
         self.api = api
         self.me = api.me()
+        self.start_time = time.time()
+        self.limit = time_limit
         
     def on_status(self, tweet):
         print(f"{tweet.user.name} just tweeted\n{tweet.text}")
         
+    def on_data(self, data):
+        if (time.time() - self.start_time) < self.limit:
+            with open("tweets_output.json", 'r') as tf:
+                file = json.load(tf)
+            json_load = json.loads(data)
+            text = json_load['text']
+            print(text)
+            file.append(text)
+            with open("tweets_output.json","w") as f:
+                json.dump(file, f, indent=4)
+            return True
+        else:
+            return False
+        
     def on_error(self, status):
         print("ERRRRRRRRRRRRRRRORRRROROROR")
-'''     
+          
 tweets_listener = MyStreamListener(api)
 stream = tweepy.Stream(api.auth, tweets_listener)
-stream.filter(track=[""], languages=["en","id"])
-'''
+stream.filter(track=["squid game"], languages=["en"])
+
+print("Proccess complete!")    
+
+"""
 imagePath = "warbler.jpg"
 status = "Hi! From my Python script =)"
 
 # Send the tweet.
 api.update_with_media(imagePath, status)    
-    
+"""
